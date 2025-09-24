995860cd618b4bddaf8a4a8b7b828acff3b14e28
NafNafra <nafafyandrianaivo@gmail.com>
2025-09-24 20:48:10 +0300

    change()

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! yeah';
+  }
+
+  startHome(id: number): string {
+    if (id >=18) return 'Home page' + id;
+    return "null"
   }
 }
