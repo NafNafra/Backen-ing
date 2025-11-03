d34a744b941fa44bac6d6d5a479ae6f6efe0f8b6
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-03 13:31:38 +0300

    Add package.json and yarn.lock for concurrently dependency

--- backend-fs/src/app.controller.ts
+++ backend-fs/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private readonly appService: AppService) { }
+  
+  @MessagePattern({ cmd: 'greet' })
+  greet(@Payload() name: string): string {
+    return this.appService.getHello(name);
   }
 }
--- backend-fs/src/app.module.ts
+++ backend-fs/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/db-1'),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
