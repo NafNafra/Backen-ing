d583b6bf1b92fbce8743e4820eeee55078d93624
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-03 13:33:35 +0300

    feat: implement microservice communication in ContactService

--- backend-main/src/modules/contact/contact.service.ts
+++ backend-main/src/modules/contact/contact.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { ContactToFs } from './dto/contact-to-fs.dto';
 import { SmsService } from 'src/commons/providers/sms/sms.service';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Injectable()
-export class ContactService {
+export class ContactService implements OnModuleInit {
+  private clientFs: ClientProxy;
+
+  onModuleInit() {
+    this.clientFs = ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 3001,
+      },
+    });
+  }
+
+  async sendMessageToFsViaMicroservice(name: string) {
+    console.log(name)
+    return this.clientFs.send({ cmd: 'greet' }, name);
+  }
+
   constructor(private readonly smsService: SmsService) { }
 
   async sendMessageToFs(contactFs: ContactToFs) {
