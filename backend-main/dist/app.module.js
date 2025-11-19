814bd69204ba17b54deeb1f6b1219a5de056538b
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-19 12:19:30 +0300

    fit

--- backend-main/dist/app.module.js
+++ backend-main/dist/app.module.js
@@ -10,8 +10,8 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const axios_1 = require("@nestjs/axios");
 const config_1 = require("@nestjs/config");
-const user_module_1 = require("./modules/user/user.module");
 const mongoose_1 = require("@nestjs/mongoose");
+const user_module_1 = require("./modules/user/user.module");
 const auth_module_1 = require("./modules/auth/auth.module");
 const sms_module_1 = require("./commons/providers/sms/sms.module");
 const configs_module_1 = require("./configs/configs.module");
@@ -19,7 +19,8 @@ const contact_module_1 = require("./modules/contact/contact.module");
 const formation_module_1 = require("./modules/formation/formation.module");
 const sessions_module_1 = require("./modules/sessions/sessions.module");
 const certificats_module_1 = require("./modules/certificats/certificats.module");
-const bridge_module_1 = require("./modules/bridge/bridge.module");
+const register_module_1 = require("./modules/register/register.module");
+const program_module_1 = require("./program/program.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -28,17 +29,17 @@ exports.AppModule = AppModule = __decorate([
         imports: [
             config_1.ConfigModule.forRoot({ isGlobal: true }),
             mongoose_1.MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/mydb'),
-            bridge_module_1.BridgeModule,
             auth_module_1.AuthModule,
+            register_module_1.RegisterModule,
             user_module_1.UsersModule,
+            formation_module_1.FormationModule,
+            sessions_module_1.SessionsModule,
             sms_module_1.SmsModule,
             configs_module_1.ConfigsModule,
             contact_module_1.ContactModule,
-            formation_module_1.FormationModule,
-            sessions_module_1.SessionsModule,
             certificats_module_1.CertificatsModule,
             axios_1.HttpModule,
-            bridge_module_1.BridgeModule,
+            program_module_1.ProgramModule,
         ],
     })
 ], AppModule);
