e17a64920f088cdd21859a97e906111361490b3d
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-20 12:40:27 +0300

    refactor: CERTIFICAT ou FORMATION. cert.formationId === payement._id payement.type == "FORMATION" ou "CERTIFICATION"

--- backend-main/src/commons/providers/fsback/fs-payement.service.ts
+++ backend-main/src/commons/providers/fsback/fs-payement.service.ts
@@ -2,8 +2,11 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigsService } from '@/configs';
 import { FsCertService } from './fs-cert.service';
-import { Types } from 'mongoose';
 import { mentionNote } from '@/commons/utils';
+import { FsCustomerService } from './fs-customer.service';
+import { FsFormationService } from './fs-formation.service';
+import { FsUserService } from './fs-user.service';
+import { Sessions } from '@/modules/sessions/entities/session.entity';
 
 @Injectable()
 export class FsPayementService {
@@ -14,7 +17,9 @@ export class FsPayementService {
     private readonly httpService: HttpService,
     private readonly configsService: ConfigsService,
     private readonly certService: FsCertService,
-
+    private readonly customer: FsCustomerService,
+    private readonly fsFormation: FsFormationService,
+    private readonly fsUser: FsUserService
   ) {
     this.url = this.configsService.get('fs_url.base');
     this.token = this.configsService.get('fs_url.token');
@@ -35,43 +40,86 @@ export class FsPayementService {
     const payments = payment.data.Payment;
 
     const certPayment = payments.filter(p =>
-      ((p.type === "CERTIFICAT") && (p.customerId !== null))
+      ((p.type === "CERTIFICAT" || p.type === "FORMATION") && (p.customerId !== null))
     );
 
     return certPayment;
   }
 
-  // certificat depuis payements
   async getCertPerPayement() {
-    const [certPayment, certificat] = await Promise.all([
+    const [payment, certificat] = await Promise.all([
       this.getCertPayement(),
       this.certService.getCertificat(),
     ]);
 
-    const combined = certPayment.map(c => ({
-      ...c,
-      certificat: certificat.filter(cp => c.targetId === cp.id),
-      mention: mentionNote(certificat[0].mention),
-      formationId: certificat[0].formationId
-    }));
+    const combined = certificat.map(c => {
+      const uniquePayment = payment.filter(cp => c.formationId === cp.id)
+      return ({
+        ...c,
+        payment: uniquePayment[0],
+        mention: mentionNote(c.mention),
+      })
+    });
 
-    console.log(combined)
+    // console.log("CERTIFICAT & PAYMENT :", combined)
     return combined;
   }
 
-  // certificat d'un etudiant depuis un payement
-  async customerCertPayment(id: string) {
-    const allCertPayment = await this.getCertPerPayement();
-    const customerCert = allCertPayment.filter(
-      certPay => {
-        if (certPay.length === 0)
-          throw new BadRequestException({ message: "Bad filtering" })
 
-        return certPay.customerId === id;
-      }
-    )
-    return customerCert
+  // customer+payment
+  async getPaymentCustomer() { // type CERTIFICAT
+    const [customers, payments] = await Promise.all([
+      this.customer.getAllCustomer(),
+      this.getCertPayement()
+    ])
+
+    const combined = payments.map(p => {
+      const uniqueCustomer = customers.filter(c => c.id === p.customerId);
+      return ({
+        ...p,
+        customers: uniqueCustomer[0],
+      })
+    });
+
+    console.log("Customer & Payment ", combined)
+    return combined;
   }
 
+  //Formation+Program+payment
+  async getSessionPayment() {
+    const [payment, sessions] = await Promise.all([
+      this.getCertPayement(),
+      this.fsFormation.getFormationsWithPrograms(),
+    ]);
 
+    const combined = payment.map(p => {
+      const uniqueSession = sessions.filter(s => s.id === p.targetId && p.targetId !== null);
+      return ({
+        ...p,
+        sessions: uniqueSession[0],
+      })
+    });
+
+    // console.log("FORMATIONS_PROGRAMS & PAYMENT :", combined)
+    return combined;
+  }
+
+  //User+Payment
+  async getUserPayment() {
+    const [payment, users] = await Promise.all([
+      this.getCertPayement(),
+      this.fsUser.getUser(),
+    ]);
+
+    const combined = payment.map(p => {
+      const uniqueUser = users.filter(u => u.id === p.userId);
+      return ({
+        ...p,
+        users: uniqueUser[0],
+      })
+    });
+
+    console.log("USER & PAYMENT :", combined)
+    return combined;
+  }
 }
