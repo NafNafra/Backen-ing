3695015c75b411d1720f47c717f64084e461327c
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-25 09:10:43 +0300

    Remove dist folder from tracking

--- backend-main/dist/configs/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { ConfigsModule } from './configs.module';
-export { ConfigsService } from './configs.service';
-export { default as configuration } from './configuration';
--- backend-main/dist/modules/formation/dto/create-formation.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateFormationDto {
-    titre: string;
-    description: string;
-    resume: string;
-    duree: string;
-}
