5a4045416ac56507099230435b83963681cef740
NafNafra <nafafyandrianaivo@gmail.com>
2025-11-25 10:11:50 +0300

    feat: get formation with their program

--- backend-main/src/modules/formation/formation.service.ts
+++ backend-main/src/modules/formation/formation.service.ts
@@ -1,26 +1,20 @@
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateFormationDto } from '@/modules/formation/dto/create-formation.dto';
-import { UpdateFormationDto } from '@/modules/formation/dto/update-formation.dto';
-import { Formation, FormationDocument } from '@/modules/formation/entities/formation.entity';
-import { FsCertService } from '@/commons/providers/fsback/fs-cert.service';
+import { Injectable } from '@nestjs/common';
+import { Formation } from '@/modules/formation/entities/formation.entity';
+import { FsFormationService } from '@/commons/providers/fsback/fs-formation.service';
 
 @Injectable()
 export class FormationService {
   constructor(
-    @InjectModel(Formation.name) private formationModel: Model<FormationDocument>,
-    private readonly fsFormation: FsCertService,
+    private readonly fsCert: FsFormationService,
   ) { }
 
-  async create(createFormationDto: CreateFormationDto): Promise<Formation> {
-    const formation = new this.formationModel(createFormationDto);
-    return formation.save();
-  }
+  // async create(createFormationDto: CreateFormationDto): Promise<Formation> {
+  //   const formation = new this.formationModel(createFormationDto);
+  //   return formation.save();
+  // }
 
-  // Formation with their programm
-  async findById(customerId: string): Promise<Formation[]> {
-    const formationProgramm = await this.fsFormation.getCustomerCertDetails(customerId);
+  async findById(formationId: string): Promise<Formation[]> {
+    const formationProgramm = await this.fsCert.getFormationById(formationId);
     return formationProgramm;
   }
 
@@ -29,38 +23,4 @@ export class FormationService {
   //   if (!formation) throw new NotFoundException('Formation introuvable');
   //   return new FormationResponseDto(formation);
   // }
-
-  async update(
-    id: string,
-    updateFormationDto: UpdateFormationDto,
-  ): Promise<Formation> {
-    const updatedFormation = await this.formationModel
-      .findByIdAndUpdate(
-        id,
-        updateFormationDto,
-        { new: true, runValidators: true },
-      )
-      .exec();
-    if (!updatedFormation) {
-      throw new NotFoundException(`Formation avec id="${id}" non trouvé`);
-    }
-    return updatedFormation;
-  }
-
-  remove(id: string): void {
-    const formationToDelete = this.formationModel.findByIdAndDelete(id).exec();
-    if (!formationToDelete)
-      throw new NotFoundException(`Formation avec id=${id} non trouvé`);
-  }
-
-  // async findOneWithSessions(id: string) {
-  //   const formationSession = this.formationModel
-  //     .findById(id)
-  //     .populate('sessions')
-  //     .exec();
-  //   if (!formationSession)
-  //     throw new NotFoundException('Formation with session not found');
-  //   return formationSession;
-  // }
-
 }
